c181b287893063624a31907ab1133079b78a982b
satyammahajan46 <satyammahajan46@gmail.com>
2019-08-22 18:07:48 -0700

    Command Change

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(_dirname+'/dist'));
+app.use(express.static(_dirname+'/dist/contactApp/'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('*/', function(req, res){
-    res.sendFile(path.join(_dirname+'/dist/index.html'));
+    res.sendFile(path.join(_dirname+'/dist/contactApp/index.html'));
 })
 
 console.log("listening");
